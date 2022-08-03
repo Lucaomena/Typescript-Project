1373098092102f1ec6e565312aa4ab8468b2bdfd
Lucaomena <lucaomena32@gmail.com>
2022-08-03 18:23:54 -0300

    primero commit

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+interface Teste {
+    nome: string,
+    idade: number
+}
+
+const object:Teste = {
+    nome:'string',
+    idade:232532
+}
---
+++ src/desafios/desafio1.js
@@ -0,0 +1,6 @@
+// Resposta 1
+const funcionario = {
+    codigo: 10,
+    nome: 'João'
+};
+
